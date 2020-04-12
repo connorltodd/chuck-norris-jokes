67ce44fa2c8e21a6126d9be2f331038b213d2ece
DEV <connortodd0304@gmail.com>
2020-04-12 22:55:01 +0100

    added footer and header

--- src/components/SharedComponents/Footer/Footer.js
+++ src/components/SharedComponents/Footer/Footer.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Footer.css';
 
 function Footer() {
     return (
-        <div>Footer</div>
+        <div className="footer">
+            <div class="navbar navbar-expand-lg navbar-light bg-primary">
+                <span class="navbar-brand text-light">Footer</span>
+            </div>
+        </div>
     )
 }
 
--- src/components/SharedComponents/Header/Header.js
+++ src/components/SharedComponents/Header/Header.js
@@ -4,9 +4,25 @@ import { Link } from 'react-router-dom';
 function Header() {
     return (
         <div>
-            <Link to="/">Home</Link>
-            <Link to="/about">About</Link>
-            <Link to="/contact">Contact</Link>
+            <nav class="navbar navbar-expand-lg navbar-light bg-primary">
+                <span class="navbar-brand text-light">Header</span>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <Link class="nav-link text-light" to="/">Home</Link>
+                        </li>
+                        <li class="nav-item">
+                            <Link class="nav-link text-light" to="/about">About</Link>
+                        </li>
+                        <li class="nav-item">
+                            <Link class="nav-link text-light" to="/contact">Contact</Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
         </div>
     )
 }
