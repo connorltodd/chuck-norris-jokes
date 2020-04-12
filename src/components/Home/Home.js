bb54df13c6aff71d2f3653652ec6348fdfc6bb40
DEV <connortodd0304@gmail.com>
2020-04-12 23:16:46 +0100

    HomeCard implemented

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,24 +1,18 @@
 import React from 'react';
+import HomeCard from './HomeCard/HomeCard';
 
 class Home extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-
         }
     }
 
+
     render() {
         return (
-            <div>
-                <div className="card" style={{ width: "18rem" }}>
-                    <img src="..." className="card-img-top" alt="..." />
-                    <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <button className="btn btn-primary">Go somewhere</button>
-                    </div>
-                </div>
+            <div className="container" style={{ paddingTop: "50px" }}>
+                <HomeCard />
             </div>
         )
     }
