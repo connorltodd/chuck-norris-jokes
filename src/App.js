83fd7668a748c5f8bc11d5225082383c666300ba
DEV <connortodd0304@gmail.com>
2020-04-12 22:29:15 +0100

    react router fully functional

--- src/App.js
+++ src/App.js
@@ -4,14 +4,19 @@ import Home from './components/Home/Home';
 import About from './components/About/About';
 import Contact from './components/Contact/Contact';
 
+import Header from './components/SharedComponents/Header/Header';
+import Footer from './components/SharedComponents/Footer/Footer';
+
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route path="/" component={Home} exact />
         <Route path="/about" component={About} />
         <Route path="/contact" component={Contact} />
       </Switch>
+      <Footer />
     </div>
   );
 }
