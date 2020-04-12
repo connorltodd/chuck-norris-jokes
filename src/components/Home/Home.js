37b8be4135e847ad0cd058fcddd79334ee15c8da
DEV <connortodd0304@gmail.com>
2020-04-12 23:52:37 +0100

    endpoints implemented as comments

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import HomeCard from './HomeCard/HomeCard';
 
+// Chuck Norris Categories Endpoint 
+// https://api.chucknorris.io/jokes/categories
+
+// Chuck Norris Joke Category Endpoint
+// https://api.chucknorris.io/jokes/random?category={category}
+
 class Home extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
         }
     }
-
 
     render() {
         return (
